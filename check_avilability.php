226ab9a8c46594e37d602de19da563d0140b822e
Wejdan20175004 <wejdano3bdulla@gmail.com>
2024-12-10 08:47:08 +0300

    Update check_avilability.php

--- check_avilability.php
+++ check_avilability.php
@@ -5,7 +5,7 @@ $roomId = $_GET['room_id'];
 $startTime = $_GET['start'];
 $endTime = $_GET['end'];
 
-// SQL to check for overlapping reservations
+
 $sql = "SELECT * FROM reservations WHERE room_id = :room_id AND 
         ((start_time < :end_time) AND (end_time > :start_time))";
 $stmt = $pdo->prepare($sql);
@@ -17,6 +17,6 @@ $stmt->execute([
 
 $reservations = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Return availability status
+
 echo json_encode(['available' => count($reservations) === 0]);
 ?>
